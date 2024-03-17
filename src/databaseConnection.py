3a53511c1ef35212c08960193a37aca1653320bb
Manal Hassan <manal.hassan@shopify.com>
2024-03-17 01:56:14 -0400

    Create the connection to the database

---
+++ src/databaseConnection.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+# Function to connect to the database
+def connect():
+    try:
+      connection = psycopg2.connect(
+          host="localhost",
+          database="School",
+          user="postgres",
+          password="plants"
+      )
+      return connection
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL", error)
+        exit(1)
+
+# Function to close the connection
+def close(con):
+    if con is not None:
+        con.close()
+
+
+
